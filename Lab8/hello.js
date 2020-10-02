b6b5eb98b79163a55a9585e41ee063362ad192a1
buimarcus <buim@hawaii.edu>
2020-10-02 13:19:41 -1000

    Lab8 + Lab9

---
+++ Lab8/hello.js
@@ -0,0 +1,29 @@
+/*
+
+var target = 10;
+var count = 0;
+
+while (count <= target) {
+    console.log(`number = ${count}`);
+
+    if (count == target / 2) break;
+    count++;
+}
+
+*/
+
+/*
+var target = 10;
+var count = 0;
+
+for (count = 0; count <= target; count++) {
+    console.log(`number = ${count}`);
+}
+
+*/
+
+var target = 10;
+
+for (count = target; count >0; count-=3) {
+    console.log("Value=" + count);
+}
---
+++ Lab9/Ex1.js
@@ -0,0 +1,11 @@
+var month = 8;
+var day = 3;
+var year = 2013;
+
+var StrYear = string(year);
+
+var lastTwo = StrYear.slice(2,3);
+
+var lastTwo = StrYear.slice(2,4);
+
+var twoDigitYear = Number(lastTwo)
