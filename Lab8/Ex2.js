316abbfb32598323d3e699ae771750f52b561bdd
buimarcus <buim@hawaii.edu>
2020-10-02 22:04:55 -1000

    Lab 8

--- Lab8/Ex2.js
+++ Lab8/Ex2.js
@@ -5,9 +5,10 @@ console.log("Silly counting program for Ex2 with Break");
 while (number < myAge) {
     
     number++;
-    if ((number >= myAge / 2) && (number <= myAge * 3/4)) {
+    if (number > myAge / 2) {
         continue;
+        
     }
     console.log("Age=" + number);
 }
-console.log("No age zone!");
+console.log("Don't ask how old I am!");
---
+++ Lab8/Ex3.js
@@ -0,0 +1,6 @@
+var number = 50;
+var myAge =20;
+
+for (number = 50; number >= myAge; number--) {
+    console.log(`Age = ${number}`)
+}
