2c7a46250345b7fbc865b0575ee7ce083297ceab
buimarcus <buim@hawaii.edu>
2020-10-16 13:19:06 -1000

    Lab11

---
+++ Lab11/Ex2.js
@@ -0,0 +1,12 @@
+var attributes  =  "Marcus;20;MIS;";
+
+var Marcus = attributes.split(";");
+
+/* for (attrib of Marcus) {
+    console.log(attrib);
+}
+*/
+
+Marcus_string = Marcus.join(",");
+
+console.log(Marcus_string);
---
+++ Lab11/example.js
@@ -0,0 +1,11 @@
+this_year = 1900;
+
+retirement_target = 2052;
+retirement_year = years_to_retire(retirement_target);
+
+console.log("I am going to retire in " + retirement_year + " years");
+
+function years_to_retire (retire_year) {
+    this_year = 2020;
+    return retire_year - this_year;
+}
